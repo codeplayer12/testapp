e2b1facb43f21f059759b6cd0e9599fddca7d34a
codeplayer12 <noblemutoko@gmail.com>
2022-05-31 07:27:18 +0200

    initial commit 2

--- test/settings.py
+++ test/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['*.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
